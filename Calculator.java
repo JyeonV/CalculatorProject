0f2e8cc0ed54f56e1ab591d88279f1d5047254aa
jungyeon <jungnam9742@gmail.com>
2025-02-26 11:16:30 +0900

    ãCalculator 클래서스 ì 생성 / ê³연산 ê 수행 기능 클래스로 이ì 전

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+package example;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    // 1. 속성
+    ArrayList<Integer> resultList = new ArrayList<>(); // 결과 저장할 컬렉션 타입 필드
+    // 2. 생성자
+
+    // 3. 기능
+    // 두 양수와 연산 기호를 매개변수로 받아서 연산 수행 후 값(i)를 반환
+    public int cal(int n1, int n2, char s1) {
+        int i = 0;
+        if (s1 == '+') {
+            i = n1 + n2;
+        } else if (s1 == '-') {
+            i = n1 - n2;
+        } else if (s1 == '*') {
+            i = n1 * n2;
+        } else if (s1 == '/') try {   // ArithmethicException 오류 발생 가능 부분
+            i = n1 / n2;
+        } catch (RuntimeException e) { // 오류 발생 시 예외 처리 부분
+            System.out.println("cannot divide by zero");
+        }
+        // 결과값을 arrayList에 추가
+        resultList.add(i);
+        return i;
+    }
+}
--- prac1.java
+++ prac1.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class prac1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int num1;
-        int num2;
+        int num1 = 0;
+        int num2 = 0;
+        char os = ' ';
         String exitInput = "";
+        // calculator 객체 생성, 값이 정확하진 않지만 미리 객체를 만들어 두는 이유는 반복문이라 객체가 계속 생성될거 같아서
+        Calculator calculator = new Calculator();
         // 양의 정수 2개를 각각 num1, num2에 전달 받기
         // 음수를 받을 경우 처음으로 돌아가서 다시 실행
         while (!exitInput.equals("exit")) { // exitInput에 exit을 제외한 기타 문자열이 들어올 시 반복
@@ -25,41 +28,28 @@ public class prac1 {
                 }
                 break; // 둘다 양수를 입력하면 반복문 탈출
             }
-            // 사칙연산 기호(+, -, *, /) 입력 받기
             // 제대로된 기호를 입력할 때 까지 반복
             // 0으로 나누기를 시도할 시 try-catch를 통한 예외 처리
             while (true) {
                 System.out.println("enter symbol(+, -, *, /) :");
-                char os = in.next().charAt(0);
-                if(os != '+' && os != '-' && os != '*' && os != '/') { // 4칙 연산 기호가 아니라면
+                os = in.next().charAt(0);
+                if(os != '+' && os != '-' && os != '*' && os != '/') { // 4칙 연산 기호가 아니라면 continue
                     System.out.println("wrong symbol");
                     continue;
                 }
-                int result = cal(num1, num2, os);
-                // 4칙연산 기호가 아니면 continue를 넣고싶다
+                // Calculator 내 메서드인 cal 호출
+                int result = calculator.cal(num1, num2, os);
                 System.out.println("result = " + result);
+                System.out.print("resultList : ");
+                // 결과값이 arrayList에 잘 쌓이는지 확인용
+                for(int a = 0; a < calculator.resultList.size(); a++) {
+                    System.out.print(" " + calculator.resultList.get(a));
+                }
                 break; // 제대로 된 입력을 했을 시 반복문 탈출
             }
             in.nextLine(); // next()에 남아있던 개행문자(엔터키) 비우기 , 버퍼 비우기
-            System.out.println("enter any key to continue \nenter 'exit' to quit");
+            System.out.println("\nenter any key to continue \nenter 'exit' to quit");
             exitInput = in.nextLine();
         }
     }
-    // 두 양수와 연산 기호를 매개변수로 받아서 연산 수행 후 값(i)를 반환
-    public static int cal(int n1, int n2, char s1) {
-        int i = 0;
-        if (s1 == '+') {
-            i = n1 + n2;
-        } else if (s1 == '-') {
-            i = n1 - n2;
-        } else if (s1 == '*') {
-            i = n1 * n2;
-        } else if (s1 == '/') try {   // ArithmethicException 오류 발생 가능 부분
-            i = n1 / n2;
-        } catch (RuntimeException e) { // 오류 발생 시 예외 처리 부분
-            System.out.println("cannot divide by zero");
-        }
-        return i;
-    }
-
 }
